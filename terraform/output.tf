be775da7eabdd514add255bb835d52e8cca99361
azazuent <azazuent@gmail.com>
2024-04-05 20:06:22 +0000

    fixes

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,6 +1,6 @@
 output "docker" {
   value = {
-    docker_ip4 = openstack_compute_instance_v2.cheque_check_docker.access_ip_v4
+    docker_server_ip4 = openstack_compute_instance_v2.cheque_check_docker.access_ip_v4
   }
 }
 
